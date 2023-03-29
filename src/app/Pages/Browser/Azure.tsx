13b18d37f77b1dca6132982baafd46a5555049a2
fkolwa <fkolwa@redhat.com>
2023-03-29 10:23:43 +0200

    Use cloudimagedirectory domain

--- src/app/Pages/Browser/Azure.tsx
+++ src/app/Pages/Browser/Azure.tsx
@@ -34,7 +34,7 @@ const Azure: React.FunctionComponent<{title: string}> = ({title}) => {
           <Title headingLevel="h1"  size={TitleSizes['4xl']}>Azure Image Browser</Title>
         </Bullseye>
       </PageSection>
-        <ImageDataTable tableColumns={columns} pathPrefix={'https://cloudx-json-bucket.s3.amazonaws.com/images/v1/idx/list/sort-by-date-azure'}/>
+        <ImageDataTable tableColumns={columns} pathPrefix={'https://poc.imagedirectory.cloud/images/v1/idx/list/sort-by-date-azure'}/>
       <PageSection >
         <Bullseye>
           <Text component={TextVariants.small}>{`Cloud Experience ${new Date().getFullYear()}.`}</Text>
