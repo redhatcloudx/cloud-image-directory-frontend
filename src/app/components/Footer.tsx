53f370cafccdb7e6018bded32e306538e7615f8e
fkolwa <fkolwa@redhat.com>
2023-05-26 10:29:20 +0200

    Baseline UX adoption for single page application layout
    Remove content from Dashboard and replace
    with basic header - hero - content - footer layout.
    Remove footer content.
    Remove custom css.
    Replace iamge logo with text logo.

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,41 +6,20 @@ import {
   TextVariants,
   TextContent,
   Grid,
-  GridItem
+  GridItem,
+  Masthead,
+  MastheadContent
 } from '@patternfly/react-core'
 import Emoji from './Emoji'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <PageSection >
-      <Bullseye>
-        <Grid hasGutter={true} component='ul'>
-          <GridItem component='li'>
-            <TextContent>
-              Can't find your favorite image <Emoji symbol="💔" /> ?
-            </TextContent>
-          </GridItem>
-          <GridItem component='li'>
-            <Bullseye>
-              <Button variant="danger">
-                <Link to='mailto:contact@imagedirectory.cloud'>
-                  <Emoji symbol="📨" /> Let us know!
-                </Link>
-              </Button>
-            </Bullseye>
-          </GridItem>
-          <GridItem component='li'>
-            <Bullseye>
-              <Text component='small'>{`Cloud Experience ${new Date().getFullYear()}.`}</Text>
-            </Bullseye>
-          </GridItem>
-        </Grid>
-      </Bullseye>
-      <Bullseye>
-        <Text component='small'>Disclaimer: This is an experimental project from Red Hat.</Text>
-      </Bullseye>
-    </PageSection>
+    <Masthead>
+      <MastheadContent>
+        This is going to be the footer
+      </MastheadContent>
+    </Masthead>
   )
 }
 
