e183f590acea19f7d4d6dde5ff67d89371ae0f4c
fkolwa <fkolwa@redhat.com>
2023-06-06 08:24:02 +0200

    Reduce routes
    Remove unnecessary browser routes
    Add dummy route for contact page

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,17 +2,11 @@ import * as React from 'react'
 import {
   Route,
   Routes,
-  useParams,
 } from 'react-router-dom'
 import {
   LastLocationProvider,
 } from 'react-router-dom-last-location'
 import { Home } from '@app/Pages/Dashboard/Home'
-import {
-    AWSImageBrowser,
-    AzureImageBrowser,
-    GCPImageBrowser
-} from '@app/Pages/Browser'
 import { NotFound } from '@app/Pages/NotFound/NotFound'
 import { useDocumentTitle } from '@app/utils/useDocumentTitle'
 import { Blog } from './Pages/Blog'
@@ -37,52 +31,41 @@ export type AppRouteConfig = IAppRoute
 
 const routes: AppRouteConfig[] = [
   {
-    component: <Home title='Cloud Image Directory | Home'/>,
+    component: <Home title='Cloud Image Directory | Home' />,
     exact: true,
     label: 'Home',
     path: '/'
   },
   {
-    component: <AWSImageBrowser title='Cloud Image Directory | AWS Image Browser'/>,
+    component: <Blog title='Cloud Image Directory | About' />,
     exact: true,
-    label: 'AWS',
-    path: '/browser/aws',
+    label: 'About',
+    path: '/about',
+  },
+  // TODO: Implement contact page
+  {
+    component: <Home title='Cloud Image Directory | Contact Us' />,
+    exact: true,
+    label: 'Contact Us',
+    path: '/contact',
   },
   {
-    component: <GCPImageBrowser title='Cloud Image Directory | GCP Image Browser'/>,
-    exact: true,
-    label: 'GCP',
-    path: '/browser/google',
-  },
-  {
-    component: <AzureImageBrowser title='Cloud Image Directory | Azure Image Browser'/>,
-    exact: true,
-    label: 'Azure',
-    path: '/browser/azure',
-  },
-  {
-    component: <ImageDetails title='Cloud Image Directory | Image Details'/>,
+    component: <ImageDetails title='Cloud Image Directory | Image Details' />,
     exact: true,
     label: 'Search',
     path: '/browser/:provider/:region/:imageName',
-  },
-  {
-    component: <Blog title='Cloud Image Directory | Blog'/>,
-    exact: true,
-    label: 'Blog',
-    path: '/Blog',
   },
 ]
 
 const AppRoutes = (): React.ReactElement => (
   <LastLocationProvider>
     <Routes>
-      {routes.map( (route, index) => {
+      {routes.map((route, index) => {
         return (
-          <Route path={route.path} element={route.component} key={`app_route_${index}`}/>
+          <Route path={route.path} element={route.component} key={`app_route_${index}`} />
         )
       })}
-      <Route path='*' element={<NotFound title='404 Page Not Found'/>} />
+      <Route path='*' element={<NotFound title='404 Page Not Found' />} />
     </Routes>
   </LastLocationProvider>
 )
