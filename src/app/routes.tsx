f3da50fde0c10c9e806b1717c76266a2c98309a2
fkolwa <fkolwa@redhat.com>
2023-06-07 07:20:01 +0200

    Remove v1 prefix from concatinated image data URL
    Maintenance fix to comply with changes in transformer
    image path construction. See redhatcloudx/transformer#585
    
    This component won't be reused in v2 of the CID front-end.

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -9,9 +9,9 @@ import {
 } from 'react-router-dom-last-location'
 import { Home } from '@app/Pages/Dashboard/Home'
 import {
-    AWSImageBrowser,
-    AzureImageBrowser,
-    GCPImageBrowser
+  AWSImageBrowser,
+  AzureImageBrowser,
+  GCPImageBrowser
 } from '@app/Pages/Browser'
 import { NotFound } from '@app/Pages/NotFound/NotFound'
 import { useDocumentTitle } from '@app/utils/useDocumentTitle'
@@ -37,37 +37,37 @@ export type AppRouteConfig = IAppRoute
 
 const routes: AppRouteConfig[] = [
   {
-    component: <Home title='Cloud Image Directory | Home'/>,
+    component: <Home title='Cloud Image Directory | Home' />,
     exact: true,
     label: 'Home',
     path: '/'
   },
   {
-    component: <AWSImageBrowser title='Cloud Image Directory | AWS Image Browser'/>,
+    component: <AWSImageBrowser title='Cloud Image Directory | AWS Image Browser' />,
     exact: true,
     label: 'AWS',
     path: '/browser/aws',
   },
   {
-    component: <GCPImageBrowser title='Cloud Image Directory | GCP Image Browser'/>,
+    component: <GCPImageBrowser title='Cloud Image Directory | GCP Image Browser' />,
     exact: true,
     label: 'GCP',
     path: '/browser/google',
   },
   {
-    component: <AzureImageBrowser title='Cloud Image Directory | Azure Image Browser'/>,
+    component: <AzureImageBrowser title='Cloud Image Directory | Azure Image Browser' />,
     exact: true,
     label: 'Azure',
     path: '/browser/azure',
   },
   {
-    component: <ImageDetails title='Cloud Image Directory | Image Details'/>,
+    component: <ImageDetails title='Cloud Image Directory | Image Details' />,
     exact: true,
     label: 'Search',
-    path: '/browser/:provider/:region/:imageName',
+    path: '/browser/v1/:provider/:region/:imageName',
   },
   {
-    component: <Blog title='Cloud Image Directory | Blog'/>,
+    component: <Blog title='Cloud Image Directory | Blog' />,
     exact: true,
     label: 'Blog',
     path: '/Blog',
@@ -77,12 +77,12 @@ const routes: AppRouteConfig[] = [
 const AppRoutes = (): React.ReactElement => (
   <LastLocationProvider>
     <Routes>
-      {routes.map( (route, index) => {
+      {routes.map((route, index) => {
         return (
-          <Route path={route.path} element={route.component} key={`app_route_${index}`}/>
+          <Route path={route.path} element={route.component} key={`app_route_${index}`} />
         )
       })}
-      <Route path='*' element={<NotFound title='404 Page Not Found'/>} />
+      <Route path='*' element={<NotFound title='404 Page Not Found' />} />
     </Routes>
   </LastLocationProvider>
 )
